68d36689ba5dff9cacc3cd3702d0be5005aae640
Cristina Semikina <cristinasemikina@gmail.com>
2022-03-29 21:19:52 +0100

    cris 29-03

---
+++ CicSeek/CicSeek/Models/util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CicSeek.Models
+{
+    public class util
+    {
+        public string utilizador { get; set; }
+        public DateTime data_nasc { get; set; }
+        public string nome { get; set; }
+        public string genero { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string foto_perf { get; set; }
+        public string biografia { get; set; }
+        public string token { get; set; }
+        public int id { get; set; }
+
+    }
+}
